fa2acef22de78d05a2b72e70a3b50fa57e461c23
Kinsey Christensen <kit.the.avenger@gmail.com>
2024-03-22 10:03:48 -0400

    created deparmtnet

---
+++ exercise/src/main/java/com/techelevator/Department.java
@@ -0,0 +1,10 @@
+package com.techelevator;
+
+public class Department {
+    private int departmentId;
+    private String name;
+
+    public Department (int departmentId, String name){}
+
+
+}//end of class
