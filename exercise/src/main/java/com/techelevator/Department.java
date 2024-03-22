e94b83b2197628c2f4a896f4413f7d580854d49d
Kinsey Christensen <kit.the.avenger@gmail.com>
2024-03-22 10:22:19 -0400

    added project

--- exercise/src/main/java/com/techelevator/Department.java
+++ exercise/src/main/java/com/techelevator/Department.java
@@ -4,7 +4,24 @@ public class Department {
     private int departmentId;
     private String name;
 
-    public Department (int departmentId, String name){}
+    //constructor
+    public Department (int departmentId, String name){
+        this.departmentId = departmentId;
+        this.name = name;
+    }
+//getters
+    public int getDepartmentId(){
+        return departmentId;
+    }
 
-
+    public String getName(){
+        return name;
+    }
+    //setters
+public void setDepartmentId(int departmentId){
+        this.departmentId = departmentId;
+}
+public void setName (String name){
+        this.name = name;
+}
 }//end of class
